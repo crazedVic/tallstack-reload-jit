2a73e2113c1ab892b3d4978dfe2944c8a072b6f1
crazedvic <victor@rubba.com>
2021-05-20 16:15:12 -0600

    added more alpine goodness

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -8,9 +8,23 @@ class Home extends Component
 {
 
     public String $message = "Welcome to Laravel Livewire with Alpine, BrowserSync and Tailwind JIT";
+    public int $countClicks = 0;
 
     public function render()
     {
         return view('livewire.home');
+    }
+
+    public function updatedMessage($value){
+        error_log("message updated " . $value);
+    }
+
+    public function updatedCountClicks($value){
+        error_log("clicks updated " . $value);
+    }
+
+    public function incrementCounter(){
+        error_log("helllo");
+        $this->countClicks = $this->countClicks + 1;
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,7 +1,16 @@
-<div class="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-400 flex flex-col w-full text-lg font-bold justify-center items-center min-h-screen" 
-    x-data="{show: false}">
-    <span>{{$message}}</span> 
-    <input class="mt-5 py-2 px-5 shadow-md rounded-sm bg-green-700 text-white" type="button" value="Hello AlpineJS" @click="show = !show">
-    <span x-show="show" class="mt-3 text-red-900">Hello back at you!</span>
-    <span class="subtitle">Using Tailwind @apply in app.scss here</span>
+<div class="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-400 flex flex-col w-full text-lg font-bold justify-center items-center min-h-screen" >
+   <span x-data="{show: false, message: @entangle('message'), countClicks: @entangle('countClicks')}"
+    class="flex flex-col w-full text-lg font-bold justify-center items-center">
+        <span>{{$message}}</span> 
+        <input class="mt-5 py-2 px-5 shadow-md rounded-sm bg-green-700 text-white" type="button" value="Hello AlpineJS" @click="show = !show">
+        <span x-show="show" class="mt-3 text-red-900">Hello back at you!</span>
+        <span class="subtitle">Using Tailwind @apply in app.scss here</span>
+        <input class="min-w-lg" type="text" x-model.debounce.500="message">
+        <input type="button" x-on:click="$wire.set('countClicks', countClicks+1)" value="Increment via AplineJS using $wire.set">
+        <input type="button" x-on:click="countClicks++" value="Increment via AplineJS">
+        <span x-text="countClicks"></span>
+        <span x-text="message"></span>
+    </span>
+    <button wire:click="incrementCounter">Increment via PHP function</button>
+    <button wire:click="$set('countClicks', {{$countClicks+1}})">Increment via livewire $set</button>
 </div>
